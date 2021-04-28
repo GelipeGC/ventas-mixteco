d9a30fc47285c8af75d03354882405af444cece7
Felipe Guzman <felipe-guzman.c@hotmail.com>
2021-04-27 22:43:06 -0500

    add migrations and models, category,product,denomination,sale,saleDetail

---
+++ app/Models/Product.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name','barcode','cost','price','stock','image','alerts','category_id'];
+
+}
---
+++ database/migrations/2021_04_28_024823_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('barcode');
+            $table->decimal('cost', $precision = 8, $scale = 2);
+            $table->decimal('price', $precision = 8, $scale = 2);
+            $table->integer('stock');
+            $table->integer('alerts')->nullable();
+            $table->string('image')->nullable();
+            $table->foreignId('category_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
